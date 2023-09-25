f6de978bc1905fdc6baf6e51e042c67d1676ae90
Penetranoob <127362922+Penetranoob@users.noreply.github.com>
2023-09-25 09:44:30 -0300

    Add files via upload

---
+++ jogodaveia/js/main.js
@@ -0,0 +1,14 @@
+// declaração de variáveis
+// var numero = 1;
+// var decimal = 1.5;
+// var texto '';
+
+let nome = '';
+const pi = 3.14;
+
+ //imprimir na tela
+ // console.log(nome);
+// console.log("Hoje não")
+
+// Declarar uma variável para representar os elementos dp jogo
+let jogo = 'x';
