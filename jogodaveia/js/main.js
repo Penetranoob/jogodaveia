d79bae01b46a1ff3cf2b3f0e78a50ffdd336c7f3
Penetranoob <127362922+Penetranoob@users.noreply.github.com>
2023-09-25 10:49:15 -0300

    Add files via upload

--- jogodaveia/js/main.js
+++ jogodaveia/js/main.js
@@ -11,4 +11,20 @@ const pi = 3.14;
 // console.log("Hoje não")
 
 // Declarar uma variável para representar os elementos dp jogo
-let jogo = 'x';
+let letra = 'X'; // X ou O
+
+function joga(casa) {
+    let celulaClicada = document.getElementById(casa).innerHTML;
+
+    if(celulaClicada == 'X' || celulaClicada == '0') {
+        alert('Esta casa já esta ocupada');
+    } else {
+        document.getElementById(casa).innerHTML = letra;
+        if(letra == 'X') {
+            letra == 'O';
+        } else {
+            letra ='X';
+        }
+    }
+}
+
